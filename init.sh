df2455ff56cd26bafe4c6dd9ae8e75979cfed7ec
Ruk <info@daco.dev>
2023-03-02 03:13:34 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,4 +11,7 @@ echo 'customize config file'
 sleep 3
 vim misskey/.config/default.yml
 docker compose -f misskey-compose.yml run --rm misskey pnpm run init
+echo 'remove depends_on as it does not work with swarm'
+sleep 3
+vim misskey-compose.yml
 docker stack deploy --with-registry-auth -c misskey-compose.yml misskey
