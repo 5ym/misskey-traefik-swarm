25e9d185c5bac8806a87c01cf7253d99b03b632b
Ruk <info@doany.io>
2023-04-13 21:04:16 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,13 +5,13 @@ wget -O misskey/.config/default.yml https://raw.githubusercontent.com/misskey-de
 wget -O misskey/.config/docker.env https://raw.githubusercontent.com/misskey-dev/misskey/develop/.config/docker_example.env
 wget https://raw.githubusercontent.com/5ym/misskey-traefik-swarm/main/misskey-compose.yml
 echo 'customize compose file'
-sleep 3
+sleep 5
 vim misskey-compose.yml
 echo 'customize config file'
-sleep 3
+sleep 5
 vim misskey/.config/default.yml
 docker compose -f misskey-compose.yml run --rm misskey pnpm run init
 echo 'remove depends_on as it does not work with swarm'
-sleep 3
+sleep 5
 vim misskey-compose.yml
 docker stack deploy --with-registry-auth -c misskey-compose.yml misskey
